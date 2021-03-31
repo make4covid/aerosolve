5f49624ef4103bd3bd6f4bc92ade97c800bee1da
wfischer42 <william.fischer42@gmail.com>
2021-03-30 18:19:18 -0600

    Create box component

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -16,17 +16,13 @@ export default {
 
 const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
+
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
   label: 'Button',
 };
-Primary.parameters = {
-   design: {
-    type: 'figma',
-    url: 'https://www.figma.com/file/i8vEpJcNHhu675LIDjAajL/Click-through-prototype?node-id=229%3A666',
-  },
-}
+
 
 export const Secondary = Template.bind({});
 Secondary.args = {

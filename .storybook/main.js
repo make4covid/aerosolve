cab5680bb938aaccb59cae5a5fb9861cce4c97ae
wfischer42 <william.fischer42@gmail.com>
2021-03-30 18:18:55 -0600

    Fix tailwind/storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -6,4 +8,20 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
   ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            plugins: [require('tailwindcss'), require('autoprefixer')],
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
+    return config;
+  },
 };

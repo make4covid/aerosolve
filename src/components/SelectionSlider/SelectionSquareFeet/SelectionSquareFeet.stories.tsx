4dd8742f2275b6c71e2127ad4640f61dacec197d
Khang <nnbaokhang@gmail.com>
2021-06-14 19:46:07 -0600

    Remove Jquery and Finalize Selection Hours

---
+++ src/components/SelectionSlider/SelectionSquareFeet/SelectionSquareFeet.stories.tsx
@@ -0,0 +1,24 @@
+import { Story } from '@storybook/react';
+import {SelectionSquareFeetProps, SelectionSquareFeet} from "./SelectionSquareFeet";
+
+export default {
+    title: 'Aerosolve/Components/SelectionSlider/SelectionSquareFeet',
+    component: SelectionSquareFeet,
+};
+
+const Template: Story<SelectionSquareFeetProps> = (args) =>
+    <div className="flex w-full h-48 bg-gray-400">
+        <div className="m-auto w-3/5">
+            <SelectionSquareFeet {...args} />
+        </div>
+    </div>
+
+
+export const SquareFeetSlider = Template.bind({})
+SquareFeetSlider.args = {
+    value:100,
+    min:100,
+    max:10000,
+    unit: "sq.feet",
+    arrayValue: [100,500,1000,5000,10000,15000]
+};

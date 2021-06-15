24577be9146650a096e9a7529f92e53721c1d063
wfischer42 <william.fischer42@gmail.com>
2021-06-15 15:11:30 -0400

    Fix PlainCard errors

--- src/components/PlainCard/PlainCard.tsx
+++ src/components/PlainCard/PlainCard.tsx
@@ -1,8 +1,6 @@
-import React, {useState} from 'react';
-import clsx from 'clsx';
-import tw from 'twin.macro';
-import {OccupantsCard, OccupantsCardProps} from "../OccupantsCard/OccupantsCard";
-import {NumberOfOccupants} from "../NumberOfOccupants/NumberOfOccupants";
+import React, { useState } from 'react'
+import { OccupantsCard } from '../OccupantsCard/OccupantsCard'
+import { NumberOfOccupants } from '../NumberOfOccupants/NumberOfOccupants'
 
 export interface CardOptions {
   occupants?: boolean
@@ -60,60 +58,35 @@ export const PlainCard: React.FC<CardProps & CardOptions> = ({ ...props }) => {
         </div>
       )}
 
-            }
-            {props.selectionHours &&
-            <div  className="flex flex-col w-full h-full p-4">
-                <div className="flex-1">
-                    <div className="flex flex-row">
-                        <p className="flex-1 text-2xl font-medium">{props.title}</p>
-                        <p className="flex-1"/>
-                        <p className="flex-1">{props.description}</p>
-                    </div>
-                </div>
-                <div className="flex-1"/>
-                <div className="flex-1 w-full h-full">
-                    {/*<SelectionSlider value={12} min={"0"} max={"24"} unit={"hours"} arrayValue={[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]}/>*/}
-                </div>
+      {props.selectionHours && (
+        <div className="flex flex-col w-full h-full p-4">
+          <div className="flex-1">
+            <div className="flex flex-row">
+              <p className="flex-1 text-2xl font-medium">{props.title}</p>
+              <p className="flex-1" />
+              <p className="flex-1">{props.description}</p>
             </div>
+          </div>
+          <div className="flex-1" />
+          <div className="flex-1 w-full h-full">
+            {/*<SelectionSlider value={12} min={"0"} max={"24"} unit={"hours"} arrayValue={[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]}/>*/}
+          </div>
+        </div>
+      )}
 
-            }
-
-            {
-                props.selectionSquareFt &&
-                <div  className="flex flex-col w-full h-full p-4">
-                    <div className="flex-1">
-                        <div className="flex flex-row">
-                            <p className="flex-1 text-2xl font-medium">{props.title}</p>
-                            <p className="flex-1"/>
-                            <p className="flex-1">{props.description}</p>
-                        </div>
-                    </div>
-                    <div className="flex-1"/>
-                    <div className="flex-1 w-full h-full">
-                        {/*<SelectionSlider value={600} min={"0"} max={">10000"} unit={"sq. feet"} arrayValue={[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]}/>*/}
-                    </div>
-                </div>
-
-            }
-
-            {
-                props.selectionHeight &&
-                <div  className="flex flex-col w-full h-full p-4">
-                    <div className="flex-1">
-                        <div className="flex flex-row">
-                            <p className="flex-1 text-2xl font-medium">{props.title}</p>
-                            <p className="flex-1"/>
-                            <p className="flex-1">{props.description}</p>
-                        </div>
-                    </div>
-                    <div className="flex-1"/>
-                    <div className="flex-1 w-full h-full">
-                        {/* <SelectionSlider value={600} min={"0"} max={">10000"} unit={"sq. feet"} arrayValue={[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]}/> */}
-                    </div>
-                </div>
-
-            }
-
+      {props.selectionSquareFt && (
+        <div className="flex flex-col w-full h-full p-4">
+          <div className="flex-1">
+            <div className="flex flex-row">
+              <p className="flex-1 text-2xl font-medium">{props.title}</p>
+              <p className="flex-1" />
+              <p className="flex-1">{props.description}</p>
+            </div>
+          </div>
+          <div className="flex-1" />
+          <div className="flex-1 w-full h-full">
+            {/*<SelectionSlider value={600} min={"0"} max={">10000"} unit={"sq. feet"} arrayValue={[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]}/>*/}
+          </div>
         </div>
       )}
 

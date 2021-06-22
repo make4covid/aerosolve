4fd610637f614c50d7baf317b6ad8466d517615a
wfischer42 <william.fischer42@gmail.com>
2021-06-22 18:39:17 -0400

    Comment out pages that use removed code

--- src/views/MaskUsage/MaskFit/MaskFit.tsx
+++ src/views/MaskUsage/MaskFit/MaskFit.tsx
@@ -1,52 +1,54 @@
-import React from 'react'
-import { StepViewProps } from 'data'
-import { SelectionChoiceGroup } from '../../../components/SelectionChoiceGroup/SelectionChoiceGroup'
-import { SelectionChoiceOption } from '../../../components/SelectionChoice/SelectionChoice'
-import { SelectionOption } from '../../../components/SelectionChoice/SelectionChoiceItem/SelectionChoiceItem'
+export {}
 
-let option: SelectionOption[] = [
-  {
-    button_description: 'Mostly No',
-  },
-  {
-    button_description: 'Average',
-  },
-  {
-    button_description: 'Mostly Yes',
-  },
-]
+// import React from 'react'
+// import { StepViewProps } from 'data'
+// // import { SelectionChoiceGroup } from '../../../components/SelectionChoiceGroup/SelectionChoiceGroup'
+// // import { SelectionChoiceOption } from '../../../components/SelectionChoice/SelectionChoice'
+// // import { SelectionOption } from '../../../components/SelectionChoice/SelectionChoiceItem/SelectionChoiceItem'
 
-let options: SelectionChoiceOption[] = [
-  {
-    options: option,
-    title: 'Occupants are wearing Masks\nthat completely covering their\nnose and mouth',
-    totalCols:5,
-    leftCol:2,
-    rightCol:3,
-    boxCols:option.length
-  },
-  {
-    options: option,
-    title: 'Occupants are wearing masks with\nmetal nosebands',
-    totalCols:5,
-    leftCol:2,
-    rightCol:3,
-    boxCols:option.length
-  },
-  {
-    options: option,
-    title: 'Occupants are wearing\ndouble masks',
-    totalCols:5,
-    leftCol:2,
-    rightCol:3,
-    boxCols:option.length
-  },
-]
+// let option: SelectionOption[] = [
+//   {
+//     button_description: 'Mostly No',
+//   },
+//   {
+//     button_description: 'Average',
+//   },
+//   {
+//     button_description: 'Mostly Yes',
+//   },
+// ]
 
-export const MaskFit: React.FC<StepViewProps> = (props) => {
-  return (
-    <div className="w-full h-3/5">
-      <SelectionChoiceGroup options={options} noDescription={true} />
-    </div>
-  )
-}
+// let options: SelectionChoiceOption[] = [
+//   {
+//     options: option,
+//     title: 'Occupants are wearing Masks\nthat completely covering their\nnose and mouth',
+//     totalCols:5,
+//     leftCol:2,
+//     rightCol:3,
+//     boxCols:option.length
+//   },
+//   {
+//     options: option,
+//     title: 'Occupants are wearing masks with\nmetal nosebands',
+//     totalCols:5,
+//     leftCol:2,
+//     rightCol:3,
+//     boxCols:option.length
+//   },
+//   {
+//     options: option,
+//     title: 'Occupants are wearing\ndouble masks',
+//     totalCols:5,
+//     leftCol:2,
+//     rightCol:3,
+//     boxCols:option.length
+//   },
+// ]
+
+// export const MaskFit: React.FC<StepViewProps> = (props) => {
+//   return (
+//     <div className="w-full h-3/5">
+//       <SelectionChoiceGroup options={options} noDescription={true} />
+//     </div>
+//   )
+// }
--- src/views/Ventilation/VentilationFiltration.tsx
+++ src/views/Ventilation/VentilationFiltration.tsx
@@ -1,96 +1,98 @@
-import React from 'react'
-import { StepViewProps } from 'data'
-import {SelectionChoiceGroup} from "../../components/SelectionChoiceGroup/SelectionChoiceGroup";
-import {SelectionOption} from "../../components/SelectionChoice/SelectionChoiceItem/SelectionChoiceItem";
-import {SelectionChoiceOption} from "../../components/SelectionChoice/SelectionChoice";
+export {}
 
-let options1 : SelectionOption[] =[
-    {
-        button_description:"Poor"
-    },
-    {
-        button_description:"Average"
-    },
-    {
-        button_description:"Good"
-    }
-]
+// import React from 'react'
+// import { StepViewProps } from 'data'
+// import {SelectionChoiceGroup} from "../../components/SelectionChoiceGroup/SelectionChoiceGroup";
+// import {SelectionOption} from "../../components/SelectionChoice/SelectionChoiceItem/SelectionChoiceItem";
+// import {SelectionChoiceOption} from "../../components/SelectionChoice/SelectionChoice";
 
-let options2 : SelectionOption[] =[
-    {
-        button_description:"None"
-    },
-    {
-        button_description:"Window A/C"
-    },
-    {
-        button_description:"Residential\nHVAC"
-    },
-    {
-        button_description:"Industrial\nHVAC"
-    },
-    {
-        button_description:"HEPA"
-    }
-]
+// let options1 : SelectionOption[] =[
+//     {
+//         button_description:"Poor"
+//     },
+//     {
+//         button_description:"Average"
+//     },
+//     {
+//         button_description:"Good"
+//     }
+// ]
 
-let options3 : SelectionOption[] =[
-    {
-        button_description:"Very Dry"
-    },
-    {
-        button_description:"Dry"
-    },
-    {
-        button_description:"Average"
-    },
-    {
-        button_description:"Humid"
-    }
-]
-let options : SelectionChoiceOption[] =[
-    {
-        options: options1,
-        title: 'Ventilation',
-        description: 'Outdoor air supply rate/\nOutdoor ACH',
-        totalCols:5,
-        leftCol:1,
-        rightCol:4,
-        boxCols:options1.length
-    },
-    {
-        options: options2,
-        title: 'Filtration System',
-        description: 'The efficiency of filtration system',
-        totalCols:5,
-        leftCol:1,
-        rightCol:4,
-        boxCols:options2.length
-    },
-    {
-        options: options1,
-        title: 'Recirculation Rate',
-        description: 'Indoor air exhaust rate/\nIndoor ACH',
-        totalCols:5,
-        leftCol:1,
-        rightCol:4,
-        boxCols:options1.length
-    },
-    {
-        options: options3,
-        title: 'Average Humidity',
-        description: 'The average outdoor relative air humidity R/H %',
-        totalCols:5,
-        leftCol:1,
-        rightCol:4,
-        boxCols:options3.length
-    },
-]
+// let options2 : SelectionOption[] =[
+//     {
+//         button_description:"None"
+//     },
+//     {
+//         button_description:"Window A/C"
+//     },
+//     {
+//         button_description:"Residential\nHVAC"
+//     },
+//     {
+//         button_description:"Industrial\nHVAC"
+//     },
+//     {
+//         button_description:"HEPA"
+//     }
+// ]
 
-export const VentilationFiltration: React.FC<StepViewProps> = (props) => {
-  return (
-    <div className="w-full h-3/5">
-     <SelectionChoiceGroup options={options} noDescription={false}/>
-    </div>
-  )
-}
+// let options3 : SelectionOption[] =[
+//     {
+//         button_description:"Very Dry"
+//     },
+//     {
+//         button_description:"Dry"
+//     },
+//     {
+//         button_description:"Average"
+//     },
+//     {
+//         button_description:"Humid"
+//     }
+// ]
+// let options : SelectionChoiceOption[] =[
+//     {
+//         options: options1,
+//         title: 'Ventilation',
+//         description: 'Outdoor air supply rate/\nOutdoor ACH',
+//         totalCols:5,
+//         leftCol:1,
+//         rightCol:4,
+//         boxCols:options1.length
+//     },
+//     {
+//         options: options2,
+//         title: 'Filtration System',
+//         description: 'The efficiency of filtration system',
+//         totalCols:5,
+//         leftCol:1,
+//         rightCol:4,
+//         boxCols:options2.length
+//     },
+//     {
+//         options: options1,
+//         title: 'Recirculation Rate',
+//         description: 'Indoor air exhaust rate/\nIndoor ACH',
+//         totalCols:5,
+//         leftCol:1,
+//         rightCol:4,
+//         boxCols:options1.length
+//     },
+//     {
+//         options: options3,
+//         title: 'Average Humidity',
+//         description: 'The average outdoor relative air humidity R/H %',
+//         totalCols:5,
+//         leftCol:1,
+//         rightCol:4,
+//         boxCols:options3.length
+//     },
+// ]
+
+// export const VentilationFiltration: React.FC<StepViewProps> = (props) => {
+//   return (
+//     <div className="w-full h-3/5">
+//      <SelectionChoiceGroup options={options} noDescription={false}/>
+//     </div>
+//   )
+// }
